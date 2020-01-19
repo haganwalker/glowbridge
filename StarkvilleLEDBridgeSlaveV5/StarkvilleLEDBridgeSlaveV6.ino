febf34d30f4e5e776ee6f87309778ea74acce953
Landon Casey <LandonCaseyCPE@Gmail.com>
2020-01-19 10:10:46 -0600

    Finish Slave Code
    Code Still needs to grab ultrasonic values and pass into the function.

--- StarkvilleLEDBridgeSlaveV5/StarkvilleLEDBridgeSlaveV6.ino
+++ StarkvilleLEDBridgeSlaveV5/StarkvilleLEDBridgeSlaveV6.ino
@@ -1,69 +1,51 @@
-#include <String.h>
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
-
+#include <M5Stack.h>
+#include <M5LoRa.h>
 
 /*Use This To Turn On/Off Pedestrian/Car detection (1 = ON | 0 = OFF)*/
 int pedestrianFunctionality = 1;
 int carFunctionality = 1;
 /********************************/
 
-/***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 0;
+/*Ultrasonic Street Variables*****************************************/
 
-RF24 radio(5, 4); // CE, CSN Pins
+int sensitivityTreshhold = 0; //Used to update the "Slop" in the sensor readings
 
-byte addresses[][6] = {"1Node","2Node"}; 
+int EndOfStreet_UValue = 0; //End of ultrasonic threshold on street
+int BeginningOfStreet_UValue=0; //Beginning of the ultrasonic threshold on street
 
-typedef enum { role_ping_out = 1, role_pong_back } role_e;                 // The various roles supported by this sketch
-const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};  // The debug-friendly names of those roles
-role_e role = role_ping_out;
+int EndOfSideWalk_UValue=0; //End of ultrasonic threshold on sidewalk
+int BeginningOfSideWalk_Uvalue=0; //Beginning of the ultrasonic threshold on sidewalk
 
+/*********************************************************************/
 
 void setup() {
+
     Serial.begin(9600);
-    
-    radio.enableAckPayload();                     // Allow optional ack payloads
-    radio.enableDynamicPayloads();                // Ack payloads are dynamic payloads
+    M5.begin();
 
-    radio.begin();
+  // override the default CS, reset, and IRQ pins (optional)
+    LoRa.setPins(); // default set CS, reset, IRQ pin
+    Serial.println("LoRa Sender");
+    M5.Lcd.println("LoRa Sender");
 
-    radio.enableAckPayload();                     // Allow optional ack payloads
-    radio.enableDynamicPayloads();                // Ack payloads are dynamic payloads
-    radio.writeAckPayload(1,1,1); 
-    
-    if(radioNumber){
-    
-        radio.openWritingPipe(addresses[1]);        // Both radios listen on the same pipes by default, but opposite addresses
-        radio.openReadingPipe(1,addresses[0]);      // Open a reading pipe on address 0, pipe 1
-    
-    }else{
-    
-        radio.openWritingPipe(addresses[0]);
-        radio.openReadingPipe(1,addresses[1]);
-    
+    if (!LoRa.begin(433E6)) {
+        Serial.println("Starting LoRa failed!");
+        M5.Lcd.println("Starting LoRa failed!");
+        while (1);
     }
-  
-    //radio.startListening(); //Ask the radio to start listenting
+
+    Serial.println("LoRa init succeeded.");
+    M5.Lcd.println("LoRa init succeeded.");
 
 }
 
 void loop(){
 
+determineAndSendObject();
 }
 
 
 void determineAndSendObject(int ultrasonicReading){ //takes in reading from the ultrasonic sensor and uses it to create a pedestrian or car code
-
-    int sensitivityTreshhold = 0; //Used to update the "Slop" in the sensor readings
-
-    int EndOfStreet_UValue = 0; //End of ultrasonic threshold on street
-    int BeginningOfStreet_UValue=0; //Beginning of the ultrasonic threshold on street
-
-    int EndOfSideWalk_UValue=0; //End of ultrasonic threshold on sidewalk
-    int BeginningOfSideWalk_Uvalue=0; //Beginning of the ultrasonic threshold on sidewalk
 
     int PedestrianCode = 111; //Code used to determine a pedestrian
     int CarCode = 222; //Code used to determine a car
@@ -92,35 +74,9 @@ void determineAndSendObject(int ultrasonicReading){ //takes in reading from the 
 
 void SendInformation(int determinationOfObject){ //send int code from slave to master and looks for an ack
     
-    if (role == role_ping_out){
-        
-        byte gotByte;                          // Initialize a variable for the incoming ACK response
-        byte informationPacket = determinationOfObject; // Send the information of the passed in var to the info packet
-        radio.stopListening();                 // First, if we are listening, stop listening so we can talk.         
-                                                        
-        if ( radio.write(&informationPacket,1) ){ // Send the Distance Packet to the other radio 
-            
-            serial.println("Successfully Sent Distance Data!");
-            
-            if(radio.available()){             //If we hear back from the radio after we send date we got an ack             
-                
-                while(radio.available() ){  // If an ack with payload was received
-                    serial.println("Ack Received!")
-                    radio.read( &gotByte, 1 );  //read the ack payload  
-                    return;                     //leave the function
-                
-                }
-            
-            }else{      //No Ack Received
-                
-                serial.println("No Ack received... That's odd...");
-                
-            }
-        
-        }else{
-            
-            serial.println("Failed To Send Distance Data!");
-        
-        } 
+    LoRa.beginPacket();
+    LoRa.print(determinationOfObject);
+    LoRa.endPacket();
+   
    } 
 }
