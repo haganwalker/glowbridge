e574f29e6ec9d0b09e6a530b9e68e23504084036
Landon Casey <LandonCaseyCPE@Gmail.com>
2020-01-19 11:14:14 -0600

    Update Slave With Debug Mode

--- StarkvilleLEDBridgeSlaveV5/V6/StarkvilleLEDBridgeSlaveV6/StarkvilleLEDBridgeSlaveV6.ino
+++ StarkvilleLEDBridgeSlaveV5/V6/StarkvilleLEDBridgeSlaveV6/StarkvilleLEDBridgeSlaveV6.ino
@@ -17,6 +17,24 @@ int EndOfSideWalk_UValue=0; //End of ultrasonic threshold on sidewalk
 int BeginningOfSideWalk_Uvalue=0; //Beginning of the ultrasonic threshold on sidewalk
 
 /*********************************************************************/
+void UltrasonicDebugMode(){
+
+    while(1){
+        M5.update();
+        M5.Lcd.clear(BLACK);
+        M5.Lcd.setCursor(0, 0);
+        M5.Lcd.setTextSize(5);
+        M5.Lcd.println(analogRead(2));
+
+        if(M5.BtnA.wasReleased()){
+            
+            M5.Lcd.clear(BLACK);
+            break;
+
+        }
+    delay(100);
+    }
+}
 
 void setup() {
 
@@ -40,10 +58,18 @@ void setup() {
 }
 
 void loop(){
-
-determineAndSendObject();
+  M5.update();
+  if(M5.BtnA.wasReleased()){
+  
+    delay(1000);
+    UltrasonicDebugMode();
+  
+  }else{
+    
+    //determineAndSendObject(1);
+  
+  }
 }
-
 
 void determineAndSendObject(int ultrasonicReading){ //takes in reading from the ultrasonic sensor and uses it to create a pedestrian or car code
 
@@ -74,9 +100,19 @@ void determineAndSendObject(int ultrasonicReading){ //takes in reading from the 
 
 void SendInformation(int determinationOfObject){ //send int code from slave to master and looks for an ack
     
+    M5.Lcd.clear(BLACK);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.setTextSize(3);
+    
+    if(determinationOfObject == 111){
+        M5.Lcd.println("Pedestrian Detected");
+    }else{
+        M5.Lcd.println("Vehicle Detected");
+    }
+    
+    M5.Lcd.println("Sending Packet!");
     LoRa.beginPacket();
     LoRa.print(determinationOfObject);
     LoRa.endPacket();
-   
-   } 
-}
+    delay(100);
+} 
